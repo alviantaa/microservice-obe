2953eec593135551fac3bd1967ad5faa900f3616
Asya Ismatullah Faris <asyafaris15@student.ub.ac.id>
2023-06-23 11:01:03 +0700

    menyesuaikan repo

--- src/grade-service/routes/api.php
+++ src/grade-service/routes/api.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::scopeBindings()->group(function () {
-    Route::apiResource('student-grades', StudentGradeController::class);
-    Route::apiResource('my-grades/{id}', MyGradeController::class);
+
 });
 
